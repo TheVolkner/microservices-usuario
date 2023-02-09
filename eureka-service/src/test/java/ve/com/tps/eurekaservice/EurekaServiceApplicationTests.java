a4ee1eb3e5ac50c9d385967ca863d0545798098f
Omar Arvelo <omararvelo81@gmail.com>
2023-02-08 21:18:22 -0400

    Eureka Adding

---
+++ eureka-service/src/test/java/ve/com/tps/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ve.com.tps.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
